682f32b6a109bb39db8839864540c2890b70c41e
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2018-07-28 22:52:20 +0200

    Added Bolt and Proton projects

---
+++ server/sp.server.node/README.md
@@ -0,0 +1,11 @@
+# spnode
+
+## Projects
+
+| Project | Task |
+| --- | --- |
+| spnode | Space Promise server node. |
+| spnode.communication.bolt | Heavy duty communication (HTTPS, FTP). |
+| spnode.communication.proton | Lightweight and fast communication (UDP). |
+| spnode.world.data | Storing the world. |
+| spnode.world.procedural | Generating the world. |
---
+++ server/sp.server.node/spnode.communication.bolt/BoltStartup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace spnode.communication.bolt
+{
+    internal class BoltStartup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public BoltStartup(IConfigurationRoot configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()));
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseCors("AllowAll");
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+        }
+    }
+}
